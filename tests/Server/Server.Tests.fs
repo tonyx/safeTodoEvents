f68a30634618c125fcfdb9f88d97acd54184739e
Tonyx <tonyx1@gmail.com>
2023-01-02 09:53:22 +0100

    clean tests

--- tests/Server/Server.Tests.fs
+++ tests/Server/Server.Tests.fs
@@ -2,20 +2,10 @@ module Server.Tests
 
 open Expecto
 
-open Shared
-open Server
-open Server.AppTests
-
-let server = testList "Server" [
-    testCase "empty" <| fun _ ->
-        Expect.isTrue true "true"
-]
-
 let all =
     testList "All"
         [
-            server
-            Server.AppTests.appTests
+            AppTests.appTests
             Shared.Tests.shared
             Shared.Tests.domain
         ]
