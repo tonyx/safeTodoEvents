16dc97b7ed9bb64f3f340377a7dc58e9f7f54ea9
Tonyx <tonyx1@gmail.com>
2023-01-14 23:51:47 +0100

    add to computation expression

--- src/Shared/Utils.fs
+++ src/Shared/Utils.fs
@@ -16,18 +16,17 @@ module Utils =
             match x with
             | Error x1 -> Error x1
             | Ok x1 -> f x1
-
         member this.MergeSources (x, y) =
             match x, y with
                 | Error x1, _ -> Error x1
                 | _, Error y1 -> Error y1
                 | Ok x1, Ok y2 -> (x1, y2) |> Ok
-
         member this.Return(x) =
             x |> Ok
-
         member this.ReturnFrom(x) =
             x
+        member this.Zero() = () |> Error
+
 
     let catchErrors f l =
         let (okList, errors) =
